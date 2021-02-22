2298d88472df8ca2f52f82b28c94505ca87cfd15
mulondumuema <mulondumuema@gmail.com>
2021-02-22 12:57:09 +0300

    finished-portfolio-example

--- rp-portfolio/personal_portfolio/projects/admin.py
+++ rp-portfolio/personal_portfolio/projects/admin.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-from blog.models import Post, Category
+# from django.contrib import admin
+# from blog.models import Post, Category
 
-class PostAdmin(admin.ModelAdmin):
-    pass
+# class PostAdmin(admin.ModelAdmin):
+#     pass
 
-class CategoryAdmin(admin.ModelAdmin):
-    pass
+# class CategoryAdmin(admin.ModelAdmin):
+#     pass
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
+# admin.site.register(Post, PostAdmin)
+# admin.site.register(Category, CategoryAdmin)
